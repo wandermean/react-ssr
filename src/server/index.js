33b4a195e06aeff82d14f655ea12064d79586c71
Zhe Chen <cz7265050@163.com>
2019-04-07 21:50:55 +0800

    端口修改

--- src/server/index.js
+++ src/server/index.js
@@ -48,4 +48,4 @@ app.get("*", function(req, res) {
   });
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3001, () => console.log("Example app listening on port 3001!"));
